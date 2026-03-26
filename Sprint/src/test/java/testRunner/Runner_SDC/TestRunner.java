6ea8fa5885b501de90bf4436f00721f326837eb7
chandrasoumya <121810090+chandrasoumya@users.noreply.github.com>
2026-03-26 23:24:01 +0530

    Day 3 delivarable added

--- Sprint/src/test/java/testRunner/Runner_SDC/TestRunner.java
+++ Sprint/src/test/java/testRunner/Runner_SDC/TestRunner.java
@@ -2,10 +2,11 @@ package testRunner.Runner_SDC;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
 
 @CucumberOptions(
-        features = {"src/test/resources/Features_SDC"},
-        glue = {"stepDefinitions.StepDefinations_SDC", "Base.Hooks_SDC"},
+        features = {"src/test/resources/FeatureFiles/Features_SDC"},
+        glue = {"stepDefinitions.StepDefinations_SDC", "Hooks.Hooks_SDC"},
         plugin = {
                 "pretty",
                 "html:target/cucumber-reports/cucumber.html",
@@ -15,4 +16,11 @@ import io.cucumber.testng.CucumberOptions;
         tags = ""
 )
 public class TestRunner extends AbstractTestNGCucumberTests {
+    
+    // This allows TestNG to run scenarios in parallel
+    @Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
 }
